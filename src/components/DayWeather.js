ce15ffde981d77fbbf4f886b120df54f09211d63
tintinkurtti <tintinkurtti@hotmail.com>
2024-07-11 15:03:55 +0200

    Added some stuff

---
+++ src/components/DayWeather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './DayWeather.css'; // Stil för den vertikala listan
+
+const DayWeather = ({ dailyData }) => {
+    if (!dailyData) {
+        // Optionally, render a loading indicator or return null
+        return <div>Loading...</div>;
+    }
+    return (
+        <div className="dailyWeather">
+            {dailyData.map((day, index) => (
+                <div key={index} className="day">
+                    <h4>{day.date}</h4>
+                    <img src={`http://openweathermap.org/img/w/${day.icon}.png`} alt="" />
+                    <p>{day.temperature}°C</p>
+                    <p>{day.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default DayWeather;
