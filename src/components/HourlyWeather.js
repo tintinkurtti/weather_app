252c89a4182df77f3d85239ded949132ddb1571f
tintinkurtti <tintinkurtti@hotmail.com>
2024-07-12 11:38:55 +0200

    Added hourly weather

---
+++ src/components/HourlyWeather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './HourlyWeather.css'; // Stil för den vertikala listan
+
+const HourlyWeather = ({ dailyData }) => {
+    if (!dailyData) {
+        // Optionally, render a loading indicator or return null
+        return <div>Loading...</div>;
+    }
+    return (
+        <div className="hourlyWeather">
+            {dailyData.map((day, index) => (
+                <div key={index} className="hour">
+                    <h4>{day.date}</h4>
+                    <img src={`http://openweathermap.org/img/w/${day.icon}.png`} alt="" />
+                    <p>{day.temperature}°C</p>
+                    <p>{day.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default HourlyWeather;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,44 +1,79 @@
 import React, { useEffect, useState, useRef } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search_icon.png'
+import DayWeather from './DayWeather';
+import HourlyWeather from "./HourlyWeather";
 
 const Weather = () => {
-
     const API_KEY = '3eee5d4598de74a916e6c327678ed871';
     const inputRef = useRef();
-    const [weatherData, setWeatherData] = useState(false);
+    const [weatherDataNow, setWeatherDataNow] = useState({});
+    const [weatherDataForecast, setWeatherDataForecast] = useState({});
+
     const search = async (city) => {
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+            const urlNow = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+            const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`;
 
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-            setWeatherData({
-                temperature: Math.floor(data.main.temp),
-                location: data.name,
-                icon: data.weather[0].icon
-            })
+            const responseNow = await fetch(urlNow);
+            const responseForecast = await fetch(urlForecast);
+
+            const dataNow = await responseNow.json();
+            const dataForecast = await responseForecast.json();
+            console.log(dataForecast);
+            console.log(dataNow);
+            setWeatherDataNow({
+                temperature: Math.floor(dataNow.main.temp),
+                location: dataNow.name,
+                icon: dataNow.weather[0].icon,
+                feels_like: Math.floor(dataNow.main.feels_like),
+                humidity: dataNow.main.humidity,
+                description: dataNow.weather[0].description,
+            });
+
+            setWeatherDataForecast({
+                daily: dataForecast.list.map((day) => {
+                    return {
+                        temperature: Math.floor(day.main.temp),
+                        icon: day.weather[0].icon,
+                        description: day.weather[0].description,
+                        date: day.dt_txt
+                    };
+                })
+            });
+
         } catch (error) {
-
+            console.log(error);
+            alert('City not found. Please try another search.');
         }
     }
 
     useEffect(() => {
-        search('London');
+        search('Stockholm');
     }, [])
-  return (
-    <div className='weather'>
-        <div className='searchbar'>
-            <input ref={inputRef} type='text' placeholder='Search...' />
-            <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
+
+    return (
+        <div className='weather'>
+            <div className='searchbar'>
+                <input ref={inputRef} type='text' placeholder='Search...'
+                       onKeyPress={(event) => {
+                           if (event.key === 'Enter') {
+                               search(inputRef.current.value);
+                           }
+                       }}/>
+                <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
+            </div>
+            <div className='weather__info'>
+                <h1>{weatherDataNow.location}</h1>
+                <h2>{weatherDataNow.temperature}°C</h2>
+                <img src={`http://openweathermap.org/img/w/${weatherDataNow.icon}.png`} alt=""/>
+                <h3>{weatherDataNow.description}</h3>
+                <p>Feels like: {weatherDataNow.feels_like}°C</p>
+                <p>Humidity: {weatherDataNow.humidity}%</p>
+            </div>
+            <HourlyWeather dailyData={weatherDataForecast.daily} />
+            <DayWeather dailyData={weatherDataForecast.daily} />
         </div>
-        <div className='weather__info'>
-            <h1>{weatherData.location}</h1>
-            <h2>{weatherData.temperature}</h2>
-            <h3>Weather</h3>
-        </div>
-    </div>
-  )
+    )
 }
 export default Weather
