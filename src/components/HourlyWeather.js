e694b2046d2d5c2a9dbd3fd303eb49ee39a6c31b
tintinkurtti <tintinkurtti@hotmail.com>
2024-08-01 14:42:46 +0200

    Update from other branch

--- src/components/HourlyWeather.js
+++ src/components/HourlyWeather.js
@@ -1,21 +1,70 @@
-import React from 'react';
-import './HourlyWeather.css'; // Stil för den vertikala listan
+import React, { useLayoutEffect, useRef } from 'react';
+import './HourlyWeather.css';
+
+const formatDate = (dateString) => {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date(dateString);
+    const dayOfWeek = days[date.getDay()];
+    return `${dayOfWeek}`;
+};
+
+const formatHour = (dateString) => {
+    const date = new Date(dateString);
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+};
 
 const HourlyWeather = ({ dailyData }) => {
+    const hourlyWeatherRef = useRef(null);
+
+    useLayoutEffect(() => {
+        const handleWheel = (event) => {
+            if (event.deltaY !== 0) {
+                event.preventDefault();
+                const scrollAmount = event.deltaY;
+                const scrollStep = scrollAmount / 20;
+                let currentScroll = 0;
+
+                const smoothScroll = () => {
+                    if (currentScroll < Math.abs(scrollAmount)) {
+                        hourlyWeatherRef.current.scrollLeft += scrollStep;
+                        currentScroll += Math.abs(scrollStep);
+                        window.requestAnimationFrame(smoothScroll);
+                    }
+                };
+
+                window.requestAnimationFrame(smoothScroll);
+            }
+        };
+
+        const hourlyWeatherElement = hourlyWeatherRef.current;
+        if (hourlyWeatherElement) {
+            hourlyWeatherElement.addEventListener('wheel', handleWheel);
+        }
+
+        return () => {
+            if (hourlyWeatherElement) {
+                hourlyWeatherElement.removeEventListener('wheel', handleWheel);
+            }
+        };
+    }, [hourlyWeatherRef]);
+
     if (!dailyData) {
-        // Optionally, render a loading indicator or return null
         return <div>Loading...</div>;
     }
+
     return (
-        <div className="hourlyWeather">
+        <ul className="hourlyWeather" ref={hourlyWeatherRef}>
             {dailyData.map((day, index) => (
-                <div key={index} className="hour">
-                    <h4>{day.date}</h4>
+                <li key={index} className="hour">
+                    <h5>{formatDate(day.date)}</h5>
+                    <h4>{formatHour(day.date)}</h4>
                     <img src={day.icon} alt="" />
                     <p>{day.temperature}°C</p>
-                </div>
+                </li>
             ))}
-        </div>
+        </ul>
     );
 };
 
---
+++ src/components/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./test.css";
+
+const Test = () => {
+    return (
+        <div className="test">
+            <h1>Test</h1>
+
+
+        </div>
+    );
+}
+
+export default Test;
